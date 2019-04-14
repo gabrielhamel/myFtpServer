9a93ea883970ae3aaaa9c46fa6cf21e6df951378
Gabriel Hamel <gabriel.hamel@epitech.eu>
2019-04-14 15:07:13 +0200

    Added padding to help

--- src/commands/help.c
+++ src/commands/help.c
@@ -6,10 +6,27 @@
 */
 
 #include <unistd.h>
+#include <string.h>
 #include "myftp.h"
 #include "responses.h"
 #include "utils.h"
 #include "commands.h"
+
+static void write_word(int fd, size_t i)
+{
+    size_t max_len = strlen(commands_g[i].name);
+
+    for (size_t j = 0; j < commands_lenght(); j++)
+        if (j % 14 == i % 14 && strlen(commands_g[j].name) > max_len)
+            max_len = strlen(commands_g[j].name);
+    max_len = max_len - strlen(commands_g[i].name) + 1;
+    write(fd, commands_g[i].name, strlen(commands_g[i].name));
+    if ((i + 1) % 14 != 0 && i != commands_lenght() - 1)
+        for (size_t j = 0; j != max_len; j++)
+            write(fd, " ", 1);
+    if (i == commands_lenght() - 1)
+        write(fd, "\r\n ", 2);
+}
 
 void command_help(socket_t *cli, socket_list_t *list, char **arg, char *path)
 {
@@ -20,16 +37,9 @@ void command_help(socket_t *cli, socket_list_t *list, char **arg, char *path)
         return;
     write(cli->fd, CODE_214_START, sizeof(CODE_214_START) - 1);
     for (size_t i = 0; i < commands_lenght(); i++) {
-        if (i == commands_lenght() - 1) {
-            write(cli->fd, commands_g[i].name, strlen(commands_g[i].name));
-            write(cli->fd, "\r\n", 2);
-            break;
-        }
         if (i % 14 == 0)
-            write(cli->fd, "\r\n ", 3);
-        write(cli->fd, commands_g[i].name, strlen(commands_g[i].name));
-        if ((i + 1) % 14 != 0)
-            write(cli->fd, " ", 1);
+            write(cli->fd, "\r\n ", 2);
+        write_word(cli->fd, i);
     }
     write(cli->fd, CODE_214_END, sizeof(CODE_214_END) - 1);
 }
