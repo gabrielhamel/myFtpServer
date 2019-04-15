b3f5df6b3ff929366c5b1cd656423f39d96aa0f8
gabrielhamel <gabriel.hamel@epitech.eu>
2019-04-15 14:54:40 +0200

    work

--- src/main.c
+++ src/main.c
@@ -11,6 +11,7 @@
 
 int main(int ac, char **av)
 {
+    setenv("LC_ALL", "en_US.UTF-8", 1);
     if (check_usage(ac, (const char **)av) == false)
         abort_prgm();
     launch_ftp(av[2], strtol(av[1], NULL, 10));
